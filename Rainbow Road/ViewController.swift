e01d953c1893fb34200f0972b347cb15596b1737
Olivier Butler <olivier.butler@gmail.com>
2017-09-12 12:56:21 -0700

    App now makes a rainbow. Wooooo!

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var raindowTable: UITableView!
+    var colours:[UIColor] = []
+    
+    func buildColours () {
+        let rgbVals:[(CGFloat,CGFloat,CGFloat)] = [
+            (241,90,90),
+            (240,196,25),
+            (177,186,114),
+            (57,214,195),
+            (51,142,219),
+            (149,91,165)
+        ]
+        for index in 0..<rgbVals.count{
+            let newCol = UIColor.init(
+                red: rgbVals[index].0 / 255,
+                green: rgbVals[index].1 / 255,
+                blue: rgbVals[index].2 / 255,
+                alpha: 1
+            )
+            self.colours.append(newCol)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buildColours()
+        raindowTable.dataSource = self
+        raindowTable.delegate = self
+        raindowTable.allowsSelection = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let protoCell = raindowTable.dequeueReusableCell(withIdentifier: "stripe", for: indexPath)
+        
+        protoCell.backgroundColor = colours[indexPath.row]
+        return protoCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        let height = raindowTable.frame.height
+        let newHeight = Int(height) / raindowTable.numberOfRows(inSection: 0)
+        return CGFloat(newHeight)
+    }
+}
